57e43a8a3ae19a0decd8a4494d380f05353edb9d
Gabriela.Queiroz <gabrielaqueirozrm@gmail.com>
2021-02-25 17:11:08 -0300

    first commit

---
+++ Js/script.js
@@ -0,0 +1,111 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+let score = 0;
+
+function createBackground() {
+    context.fillStyle = "#010000";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function createSnake() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "#A6FC34";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function drawFood() {
+    context.fillStyle = "#F30203";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function startGame() {
+
+    if (snake[0].x > 15 * box && direction == "right" || snake[0].x > 15 * box && direction == "up" || snake[0].x > 15 * box && direction == "down") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left" || snake[0].x < 0 && direction == "up" || snake[0].x < 0 && direction == "down") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down" || snake[0].y > 15 * box && direction == "right" || snake[0].y > 15 * box && direction == "left") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up" || snake[0].y < 0 && direction == "right" || snake[0].y < 0 && direction == "left") snake[0].y = 16 * box;
+
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(game);
+            alert("Game Over!");
+        }
+    }
+
+    createBackground();
+    createSnake();
+    drawFood();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+        score++;
+        console.log(score);
+        var input = document.getElementById("score");
+        input.value = score;
+    }
+
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
+
+var activities = document.getElementById("level");
+activities.addEventListener("change", function () {
+    let level = activities.options[activities.selectedIndex].value;
+    if (level == "easy") {
+        clearInterval(game);
+        game = setInterval(startGame, 150);
+        console.log(level);
+        console.log(game);
+    } else if (level == "normal") {
+        clearInterval(game);
+        game = setInterval(startGame, 100);
+        console.log(level);
+        console.log(game);
+    } else {
+        clearInterval(game);
+        game = setInterval(startGame, 50);
+        console.log(level);
+        console.log(game);
+    }
+})
